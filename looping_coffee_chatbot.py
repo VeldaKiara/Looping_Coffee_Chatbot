d6379bfbdd02f20644bc4d21a7bfbe9714b34732
Velda Kiara <veldakarimi@gmail.com>
2020-05-22 11:46:05 +0300

    addition of drinks

--- looping_coffee_chatbot.py
+++ looping_coffee_chatbot.py
@@ -3,11 +3,13 @@ from utils import print_message, get_size, order_latte
 def coffee_bot():
   print('Welcome to the cafe!')
   order_drink = 'y'
+  drinks = []
   while order_drink == 'y':
     size = get_size()  
     drink_type = get_drink_type()
     drink = '{} {}'.format(size, drink_type)
     print('Alright, that\'s a {}!'.format(drink))
+    drinks.append(drink)
     while True: 
       order_drink = input("Would you like to order another drink? (y/n) \n>")
   name = input('Can I get your name please? \n> ')
